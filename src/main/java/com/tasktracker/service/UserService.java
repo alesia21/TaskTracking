e7afa08e5b93908ad244c7464d7161be7b74755c
alesia21 <102740521+alesia21@users.noreply.github.com>
2025-05-30 14:47:32 +0200

    Update UserService.java
    changes

--- src/main/java/com/tasktracker/service/UserService.java
+++ src/main/java/com/tasktracker/service/UserService.java
@@ -2,7 +2,6 @@ package com.tasktracker.service;
 
 import com.tasktracker.entity.User;
 import com.tasktracker.repository.UserRepository;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
@@ -12,22 +11,28 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
 
+   
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
+  
     public List<User> listUsers(int page, int size) {
         return userRepository
-                .findAll(PageRequest.of(page, size))
+                .findAll(org.springframework.data.domain.PageRequest.of(page, size))
                 .getContent();
     }
+
 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
 
 
-    public User createUser(User user) {
+
+    public User saveUser(User user) {
         return userRepository.save(user);
     }
 }
